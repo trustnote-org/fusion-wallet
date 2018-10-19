c3581c0bf7dbc11d2c8cb4a561b423e80ace9d2c
kakegu <gusuncheng@gmail.com>
2018-10-19 17:03:49 +0800

    :zap: replace Promise with async/await

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -4,8 +4,6 @@ import { resultType } from "./types.service";
 import { ResultService } from "./result.service";
 import { NGXLogger } from "ngx-logger";
 
-import _ from "lodash";
-
 export enum StorageKeys {
   WALLET = "wallet",
   SETTINGS = "settings",
@@ -23,45 +21,47 @@ export class StorageService {
   constructor(public storage: Storage, private result: ResultService, private logger: NGXLogger) { }
 
   processingData(v) {
-    if (!v) return null;
-    if (!_.isString(v)) return v;
+    if (!v)
+      return null;
+    if (typeof v == "object")
+      return v;
     let parsed;
     try {
       parsed = JSON.parse(v);
     } catch (e) {
-      this.logger.error("");
+      this.logger.error(e);
     }
-    return parsed || v;
+    return parsed || null;
   }
 
-  set(key: string, value: any): Promise<resultType> {
-    return this.storage.set(key, value).then(
-      success => {
-        this.logger.info("setStorage:", key, "->", success);
-        return this.result.success(success);
-      },
-      error => {
-        return this.result.error(error);
-      }
-    );
+  async set(key: StorageKeys, value: any): Promise<resultType> {
+    try {
+      let ret = await this.storage.set(key, value);
+      this.logger.debug("set: ", ret);
+      return this.result.success(ret);
+    } catch (error) {
+      throw this.result.error(error);
+    }
   }
 
-  get(key: string): Promise<any> {
-    return new Promise((resolve, rejects) => {
-      this.storage.get(key).then(
-        value => {
-          return resolve(this.processingData(value));
-        },
-        error => {
-          rejects(this.result.error(error));
-        }
-      );
-    });
+  async get(key: StorageKeys): Promise<any> {
+    try {
+      let ret = await this.storage.get(key);
+      this.logger.debug("get: ", ret);
+      let value = this.processingData(ret);
+      return value;
+    } catch (error) {
+      throw this.result.error(error);
+    }
   }
 
-  remove(key: string) {
-    this.storage.remove(key).then(ret => {
-      this.logger.info("removeStorage:", key, "->", ret);
-    });
+  async remove(key: StorageKeys): Promise<resultType> {
+    try {
+      let ret = await this.storage.remove(key);
+      this.logger.debug("remove: ", ret);
+      return this.result.success(ret);
+    } catch (error) {
+      throw this.result.error(error);
+    }
   }
 }
