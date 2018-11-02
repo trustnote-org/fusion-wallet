caf49cf430ff2ae9e34d8b5240b627852a82fe22
kakegu <gusuncheng@gmail.com>
2018-11-02 17:51:13 +0800

    :bug: fixed history list

--- src/app/pages/account/account.page.ts
+++ src/app/pages/account/account.page.ts
@@ -14,15 +14,11 @@ import { NGXLogger } from 'ngx-logger';
   styleUrls: ['./account.page.scss']
 })
 export class AccountPage implements OnInit {
-  addr: string;
+  address: string;
   history: historyType;
-  // historyStore: historyType;
-  historyArr: Array<any>;
-  tit: string;
-  date: any;
-  amount: any;
-  unit: string;
+  arrHistory: Array<any>;
   hasHistory: boolean;
+  isUpdating: boolean;
 
   constructor(
     private logger: NGXLogger,
@@ -30,112 +26,94 @@ export class AccountPage implements OnInit {
     private profile: ProfileService,
     private factory: FactoryService
   ) {
+    this.address = this.profile.wallet.address;
     this.history = {};
-    // this.historyStore = {};
-    this.historyArr = [];
+    this.hasHistory = false;
+    this.arrHistory = [];
+    this.isUpdating = false;
   }
 
   ngOnInit() {
     // 本地存储 账单 (首先加载本地历史)
     this.profile.loadHistory().then(
-      ret => {
-        if (ret && Object.keys(ret).length > 0) {
+      history => {
+        if (history && Object.keys(history).length > 0) {
           this.hasHistory = true;
-          for (const key of Object.keys(ret)) {
-            // console.log(ret[key]);
-            if (ret[key].action === Action.RECEIVED) {
-              // this.tit = 'from ' + ret[key].from[0];
-              this.tit = '收款';
-            }
-            if (ret[key].action === Action.SENT) {
-              // this.tit = 'to ' + ret[key].to[0];
-              this.tit = '转账';
-            }
-            this.date = this.formatDateTime(ret[key].timestamp);
-            this.amount = ret[key].amount.TTT;
-            this.unit = ret[key].unit;
+          this.history = history;
+          let action: string;
 
-            const obj = { tit: this.tit, date: this.date, amount: this.amount, unit: this.unit };
-            this.historyArr.push(obj); // 前端页面遍历的 数组
+          for (const key of Object.keys(history)) {
+            if (history[key].action === Action.RECEIVED) {
+              action = '收款';
+            }
+            if (history[key].action === Action.SENT) {
+              action = '转账';
+            }
+
+            const obj = {
+              action: action,
+              date: this.formatDateTime(history[key].timestamp),
+              amount: history[key].amount.TTT,
+              unit: history[key].unit
+            };
+
+            this.arrHistory.push(obj); // 前端页面遍历的 数组
           }
-        } else {
-          this.hasHistory = false;
         }
+        this.updateHistory();
       },
-      reject => {
-        this.logger.info('加载本地历史记录出错', reject);
+      error => {
+        this.logger.error(error);
       }
     );
-    // console.log(this.historyArr);
+  }
 
-    // this.addr = 'ZDKNB2DQJPQR7PKYI37A5M2MTU5SIZ2A';
-    this.addr = this.profile.wallet.address;
-    this.network.getHistory(this.addr, 'TTT', 1, 5).subscribe(
+  updateHistory() {
+    this.isUpdating = true;
+    this.network.getHistory(this.address, 'TTT', 1, 10).subscribe(
       res => {
-        // console.log(this.factory.unpackUnit(res.data.history[0].unit));
-        // console.log(res.data.history);
-        res.data.history.forEach(item => {
-          let tx = this.factory.unpackUnit(item);
-          this.history[tx.unit] = _.clone(tx);
-        });
+        if (res.data) {
+          res.data.history.forEach(item => {
+            let tx = this.factory.unpackUnit(item);
+            this.history[tx.unit] = _.clone(tx);
+          });
 
-        // let historyList = [];
-        // for (const key of Object.keys(this.history)) {
-        //   historyList.push(this.history[key]);
-        // }
-        // historyList = historyList.sort(this.compare);
-        // // console.log('----------', historyList);
-        // for (let i = 0; i < historyList.length; i++) {
-        //   this.historyStore[historyList[i].unit] = _.clone(historyList[i]);
-        // }
-        // console.log('****************************');
-        // console.log(this.historyStore);
-        // for (const key of Object.keys(this.historyStore)) {
-        //   console.log(this.historyStore[key]);
-        // }
+          let tempArrHistory = [];
+          let action: string;
 
-        this.profile.storeHistory(this.history).then(
-          ret => {
-            this.profile.loadHistory().then(
-              ret => {
-                if (Object.keys(ret)[0] === this.historyArr[0].unit) {
-                  return;
-                }
-                for (const key of Object.keys(ret)) {
-                  // console.log(ret[key]);
-                  if (ret[key].action === Action.RECEIVED) {
-                    // this.tit = 'from ' + ret[key].from[0];
-                    this.tit = '收款';
-                  }
-                  if (ret[key].action === Action.SENT) {
-                    // this.tit = 'to ' + ret[key].to[0];
-                    this.tit = '转账';
-                  }
-                  this.date = this.formatDateTime(ret[key].timestamp);
-                  this.amount = ret[key].amount.TTT;
-                  this.unit = ret[key].unit;
+          for (const key of Object.keys(this.history)) {
+            if (this.history[key].action === Action.RECEIVED) {
+              action = '收款';
+            }
+            if (this.history[key].action === Action.SENT) {
+              action = '转账';
+            }
 
-                  const obj = {
-                    tit: this.tit,
-                    date: this.date,
-                    amount: this.amount,
-                    unit: this.unit
-                  };
-                  this.historyArr.push(obj); // 前端页面遍历的 数组
-                }
-              },
-              reject => {
-                this.logger.info('二次读取历史出错');
-              }
-            );
-          },
-          err => {
-            this.logger.info('存储历史出错');
+            const obj = {
+              action: action,
+              date: this.formatDateTime(this.history[key].timestamp),
+              amount: this.history[key].amount.TTT,
+              unit: this.history[key].unit
+            };
+            tempArrHistory.push(obj);
           }
-        );
+
+          this.arrHistory = tempArrHistory;
+          this.isUpdating = false;
+
+          this.profile.storeHistory(this.history).then(
+            ret => {
+              this.logger.debug(ret);
+            },
+            err => {
+              this.logger.error(err);
+            }
+          );
+        }
       },
       err => {
-        this.logger.info('网络获取历史记录出错');
+        this.isUpdating = false;
+        this.logger.error(err);
       }
     );
   }
@@ -144,18 +122,7 @@ export class AccountPage implements OnInit {
   goBack() {
     history.go(-1);
   }
-  // 排序
-  compare(obj1, obj2) {
-    let val1 = obj1.timestamp;
-    let val2 = obj2.timestamp;
-    if (val1 < val2) {
-      return 1;
-    } else if (val1 > val2) {
-      return -1;
-    } else {
-      return 0;
-    }
-  }
+
   // 时间戳 转换
   formatDateTime(inputTime) {
     let date = new Date(inputTime * 1000);
