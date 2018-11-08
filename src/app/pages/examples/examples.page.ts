103b905db43cb3934c46bd0f1f09dfdbb9a6928b
gaoyang <2313332818@qq.com>
2018-11-08 10:01:30 +0800

    update payment

--- src/app/pages/examples/examples.page.ts
+++ src/app/pages/examples/examples.page.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { PaymentPage } from '../../modal/payment/payment.page';
-import { ModalController } from '@ionic/angular';
-
 import { PaymentService } from '../../services/payment.service';
 
 @Component({
@@ -14,28 +11,10 @@ import { PaymentService } from '../../services/payment.service';
 export class ExamplesPage implements OnInit {
   constructor(
     private router: Router,
-    public modalController: ModalController,
     private paymentService: PaymentService
   ) {}
 
   ngOnInit() {}
-
-  customLeaveAnimation(AnimationC, baseEl) {
-    const baseAnimation = new AnimationC();
-    const backdropAnimation = new AnimationC();
-    backdropAnimation.addElement(baseEl.querySelector('ion-backdrop'));
-    const wrapperAnimation = new AnimationC();
-    wrapperAnimation.addElement(baseEl.querySelector('.paymentModal'));
-    backdropAnimation.fromTo('opacity', 0.5, 0);
-    wrapperAnimation.fromTo('translateY', '0%', '100%');
-    const ani = baseAnimation
-      .addElement(baseEl)
-      .easing('cubic-bezier(.36,.66,.04,1)')
-      .duration(250)
-      .add(backdropAnimation)
-      .add(wrapperAnimation);
-    return Promise.resolve(ani);
-  }
 
   // 返回 主页
   goBack() {
@@ -45,19 +24,7 @@ export class ExamplesPage implements OnInit {
   openOnBrowser() {
     this.router.navigate(['/browser']);
   }
-
-  // // 打开 模态框
-  // async presentModal() {
-  //   const modal = await this.modalController.create({
-  //     component: PaymentPage,
-  //     leaveAnimation: this.customLeaveAnimation,
-  //     showBackdrop: true,
-  //     backdropDismiss: true
-  //     // componentProps: { value: 123 }
-  //   });
-  //   return await modal.present();
-  // }
-
+  // 点击 付款
   presentModal() {
     this.paymentService.confirmPay('KPQ3CRPBG5FSKVEH6Y76ETGD5D2N7QZ7', 123, 'TTT', 'hello');
   }
