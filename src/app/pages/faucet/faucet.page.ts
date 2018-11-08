e3f369e8e7b2b548461e66d6772c6b84ef7f90e9
kakegu <gusuncheng@gmail.com>
2018-11-08 17:58:22 +0800

    update getCoin status

--- src/app/pages/faucet/faucet.page.ts
+++ src/app/pages/faucet/faucet.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
 
 import { NGXLogger } from 'ngx-logger';
+import { ProfileService } from '../../services/profile.service';
 import { NetworkService } from '../../services/network.service';
 import { TipsService } from '../../services/tips.service';
 
@@ -11,12 +12,17 @@ import { TipsService } from '../../services/tips.service';
   styleUrls: ['./faucet.page.scss']
 })
 export class FaucetPage implements OnInit {
+  isGotCoin: boolean;
+
   constructor(
+    private profileService: ProfileService,
     private networkService: NetworkService,
     public loadingController: LoadingController,
     private tipsService: TipsService,
     private logger: NGXLogger
-  ) {}
+  ) {
+    this.isGotCoin = this.profileService.status.isGotCoin;
+  }
 
   ngOnInit() {}
 
@@ -26,6 +32,12 @@ export class FaucetPage implements OnInit {
       success => {
         this.loadingController.dismiss();
         this.tipsService.alert('领取成功', null);
+        if (!this.isGotCoin) {
+          this.isGotCoin = true;
+          let status = this.profileService.status;
+          status.isGotCoin = true;
+          this.profileService.storeStatus(status);
+        }
         this.logger.debug(success);
       },
       error => {
