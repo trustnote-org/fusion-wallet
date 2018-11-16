2bfbf50f4990e85b19cf571156d217279163cb2b
gaoyang <2313332818@qq.com>
2018-11-16 16:18:17 +0800

    update miniapp progress

--- src/app/pages/browser/browser.page.ts
+++ src/app/pages/browser/browser.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   selector: 'app-browser',
@@ -18,74 +19,75 @@ export class BrowserPage implements OnInit {
     url: '',
     share: null // 是否具有分享功能（传递一个分享对象ShareModel过来）
   };
+
+  // 分享控制的配置
   shareConfig: any = {
     isShow: false
-  }; // 分享控制的配置
+  };
 
+  // 是否 直接从home页面过来
   fromHome = false;
 
-  constructor(private sanitizer: DomSanitizer, private translate: TranslateService, private router: Router, private activate: ActivatedRoute) {
-    // let browser = this.params.get('browser');
-    // let url = this.params.get('url');
-    // let title = this.params.get('title');
-    // let unit = this.params.get('unit');
+  constructor(
+    private sanitizer: DomSanitizer,
+    private translate: TranslateService,
+    private router: Router,
+    private activate: ActivatedRoute,
+    private logger: NGXLogger
+  ) {
     this.activate.queryParams.subscribe(params => {
-      console.log('-------*********', params);
+      this.logger.info('-------*********', params);
       this.browser.title = params.title;
       this.browser.url = params.url;
       if (params.fromHome) {
         this.fromHome = params.fromHome;
       }
     });
-
-    // this.browser.url = 'https://github.com/trustnote';
-
-    // if (unit) {
-    //   this.browser.url = `https://testexplorer.trustnote.org/detail#${unit}`;
-    // }
-
     this.browser.secUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.browser.url);
-    // this.reload();
+    this.reload();
   }
 
   ngOnInit() {
     if (!this.browser.proObj) {
       this.browser.proObj = document.getElementById('progress');
     }
-    // this.onprogress();
+    this.onprogress();
   }
+
   // 生成随机数
   private random(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   // 网页访问进度
-  // private onprogress() {
-  //   // 随机时间
-  //   let timeout = this.random(10, 30);
+  private onprogress() {
+    // 随机时间
+    let timeout = this.random(10, 30);
 
-  //   let timer = setTimeout(() => {
-  //     if (this.browser.isLoaded) {
-  //       this.browser.proObj.style.width = '100%';
-  //       clearTimeout(timer);
-  //       return;
-  //     }
+    let timer = setTimeout(() => {
+      if (this.browser.isLoaded) {
+        this.browser.proObj.style.width = '100%';
+        clearTimeout(timer);
+        return;
+      }
 
-  //     // 随机进度
-  //     this.browser.progress += this.random(1, 5);
+      // 随机进度
+      this.browser.progress += this.random(1, 5);
 
-  //     // 随机进度不能超过 90%，以免页面还没加载完毕，进度已经 100% 了
-  //     if (this.browser.progress > 90) {
-  //       this.browser.progress = 90;
-  //     }
+      // 随机进度不能超过 90%，以免页面还没加载完毕，进度已经 100% 了
+      if (this.browser.progress > 90) {
+        this.browser.progress = 90;
+      }
 
-  //     this.browser.proObj.style.width = this.browser.progress + '%';
-  //     this.onprogress();
-  //   }, timeout);
-  // }
+      this.browser.proObj.style.width = this.browser.progress + '%';
+      this.onprogress();
+    }, timeout);
+  }
+
   // 如果iframe页面加载成功后
   loaded() {
     this.browser.isLoaded = true;
   }
+
   // 关闭浏览器页面
   closePage(myEvent) {
     if (this.fromHome) {
@@ -94,6 +96,7 @@ export class BrowserPage implements OnInit {
       this.router.navigate(['/examples']);
     }
   }
+
   // 重新加载页面
   reload() {
     let title = this.browser.title;
@@ -107,7 +110,7 @@ export class BrowserPage implements OnInit {
       if (!this.browser.proObj) {
         this.browser.proObj = document.getElementById('progress');
       }
-      // this.onprogress();
+      this.onprogress();
       this.browser.title = title;
       this.browser.secUrl = url;
     }, 10);
